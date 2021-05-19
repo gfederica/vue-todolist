52e0271a8ccced437135cad1a4391557c45ad1a0
gfederica <effegiudice@gmail.com>
2021-05-19 16:05:25 +0200

    Esercizio base completato

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// Creare con Vue una to-do list, in cui ogni elemento è un oggetto composto da:
+// - testo
+// - "completed", un booleano settato inizialmente a false.
+// Stampare in pagina un item per ogni elemento della to-do list.
+// Ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista.
+// Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
+
+
+var app = new Vue ({
+    el : '#root',
+    data : {
+       toDoList : [
+        {
+            testo: "Fare la spesa",
+            completed: false
+        },
+        {
+            testo: "Controllare le e-mail",
+            completed: false
+        },
+        {
+            testo: "Studiare Vue JS",
+            completed: false
+        },
+        {
+            testo: "Innaffiare le piante",
+            completed: false
+        },      
+        {
+            testo: "Programmare le vacanze",
+            completed: false
+        }          
+       ],
+       newToDo : {
+           testo: "",
+           completed: false
+       }
+    },
+    methods : {
+        removeToDo: function(index) {
+        this.toDoList.splice(index, 1);
+        },
+        addToDo: function () {
+            this.toDoList.push(this.newToDo);
+            this.newToDo = {
+                testo: ""
+            };
+        }
+    }
+}
+)
