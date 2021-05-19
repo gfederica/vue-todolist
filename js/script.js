44166fc9be67599a33b100466f96379b2ab47f6b
gfederica <effegiudice@gmail.com>
2021-05-19 17:23:33 +0200

    Completato + bonus

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@
 // Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure cliccando su un pulsante, il testo digitato viene aggiunto alla lista (all'interno di un nuovo oggetto con proprietà "completed" settata a false).
 
 
+// BONUS:
+// Al click sull'intero elemento della lista, si modifica il valore della proprietà completed da false a true, e viceversa in caso di successivi click (toggle).
+// Quando un elemento ha la proprietà "completed" settata a true, il testo corrispondente sarà mostrato barrato
+
 var app = new Vue ({
     el : '#root',
     data : {
@@ -45,7 +49,13 @@ var app = new Vue ({
             this.newToDo = {
                 testo: ""
             };
+        },
+        toggleCompleted: function (index) {
+            this.toDoList[index].completed = !this.toDoList[index].completed;
+            }
         }
-    }
 }
 )
+
+
+ 
